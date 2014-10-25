ad4f4eec2b596a28dc3fd08cfbcae03a7de46920
Nicolas Papagna Maldonado <nicolas.papagna@8thlight.com>
2014-10-25 02:07:13 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+The Closure Challenge
+=====================
+
+The goal is to implement the :build_return_block_from method defined in closure_challenge_spec.rb so all tests pass.
+
+Seems easy... why don't you give closures a try?
+
+Rules
+=====
+
+You can only use the Ruby core library.
+The only gem you can use, if you need to, is Binding of Caller.
+Your solution should run on MRI and JRuby.
+
+Good luck!
---
+++ closure_challenge_spec.rb
@@ -0,0 +1,41 @@
+describe 'The Closure Challenge' do
+
+  def build_return_block_from a_block
+    fail 'implement me'
+  end
+
+  def evaluate_and_return a_block
+    return_block = build_return_block_from a_block
+    return_block.call
+
+    fail "this line shouldn't be executed"
+  end
+
+  def evaluate_with_args_and_return a_number, a_block
+    return_block = build_return_block_from a_block
+    return_block.call a_number
+
+    fail "this line shouldn't be executed"
+  end
+
+  it 'returns from the calling method with the value returned from a proc' do
+    result = evaluate_and_return proc{ 7 }
+
+    result.should == 7
+  end
+
+  it 'returns from the calling method with the non-primitive value returned from a proc' do
+    expected_result = Object.new
+
+    result = evaluate_and_return proc{ expected_result }
+
+    result.should == expected_result
+  end
+
+  it 'support arguments to be passed to the block' do
+    result = evaluate_with_args_and_return 1, proc{ |number| number + 1 }
+
+    result.should == 2
+  end
+
+end
